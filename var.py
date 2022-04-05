3584f59d9aebfddfb1288393648e486554d54cb3
Pablo <73362636+PabloGO4@users.noreply.github.com>
2022-04-05 13:06:05 +0200

    version 0.1

---
+++ var.py
@@ -0,0 +1,24 @@
+'''
+Fichero de variables globales
+'''
+
+global ui
+global dlgaviso
+global dlgacercade
+global dlgCalendar
+global dlgabrir
+global copia
+global cv
+global btnfacdel
+global cmbproducto
+global txtCantidad
+global precio
+global codpro
+global subfac
+global iva
+global fac
+
+'''
+Otras variables
+'''
+filedb = 'bbdd.sqlite'
